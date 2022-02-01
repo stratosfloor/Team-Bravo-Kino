7e80a7da37dd32d3e94c2edb4d6bb9f2cb0329e7
Chrikelin <Christer.resin@gmail.com>
2022-02-01 21:27:28 +0100

    remove limit of reviews from API with qs pagination

--- src/reviews.js
+++ src/reviews.js
@@ -1,12 +1,14 @@
 import fetch from 'node-fetch';
 
-const API_URL = `https://lernia-kino-cms.herokuapp.com/api/reviews`
+const API_URL = `https://lernia-kino-cms.herokuapp.com/api/reviews`;
 
 const loadMovieReviews = async (movieId) => {
-  const res = await fetch(`${API_URL}?filters[movie]=${movieId}`);
+  const res = await fetch(
+    `${API_URL}?filters[movie]=${movieId}&pagination[pageSize]=1000`
+  );
   const payload = await res.json();
 
-  return payload.data
-}
+  return payload.data;
+};
 
 export default loadMovieReviews;
