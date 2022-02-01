b2db5c8485b4939f818244ca96520e6d0d8310a3
Chrikelin <Christer.resin@gmail.com>
2022-02-01 18:56:02 +0100

    refactor logic for buttons and and pagination function

--- static/reviews.js
+++ static/reviews.js
@@ -1,99 +1,88 @@
 const renderReviews = async () => {
-
   const loadReviws = async () => {
     const url = window.location.href;
     const urlArr = url.split('/');
     const movieId = urlArr[urlArr.length - 1];
-  
+
     try {
-      const URL = `/api/movies/${movieId}/reviews`
+      const URL = `/api/movies/${movieId}/reviews`;
       const response = await fetch(URL);
       const payload = await response.json();
-      console.log(payload);
       return payload;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const container = document.querySelector('.movieReviewsContainer');
   const reviewsList = document.querySelector('.movieReviewsList');
 
   const reviewsData = await loadReviws();
-  const page_size = 2;
-  let page_number = 1;
-  
+  const page_size = 5;
+  let page_number = 0;
+
   function pagination(array, page_size, page_number) {
-    return array.slice((page_number -1) * page_size, page_number * page_size)
-  };
+    return array.slice((page_number - 1) * page_size, page_number * page_size);
+  }
 
   const renderReviews = (direction) => {
-    const data = pagination(reviewsData, page_size, page_number);
-    if(direction === 'forward') {
-      page_number++
-      console.log(page_number)
+    if (direction === 'forward') {
+      page_number++;
     } else if (direction === 'back') {
-      page_number--
-      console.log(page_number);
+      page_number--;
     }
 
-    reviewsList.innerHTML = ''
+    const data = pagination(reviewsData, page_size, page_number);
+
+    reviewsList.innerHTML = '';
 
     data.forEach((review) => {
       const li = document.createElement('li');
-      
-        const rating = document.createElement('div');
-        rating.innerHTML = `Betyg: ${review.attributes.rating}`;  
-        li.appendChild(rating);
-      
-      
-      if(review.attributes.comment) {
+
+      const rating = document.createElement('div');
+      rating.innerHTML = `Betyg: ${review.attributes.rating}`;
+      li.appendChild(rating);
+
+      if (review.attributes.comment) {
         const comment = document.createElement('div');
         comment.innerHTML = `Kommenter: ${review.attributes.comment}`;
         li.appendChild(comment);
       }
-  
-      if(review.attributes.author) {
+
+      if (review.attributes.author) {
         const author = document.createElement('div');
-        author.innerHTML = `Lämnad av: ${review.attributes.author}`;  
+        author.innerHTML = `Lämnad av: ${review.attributes.author}`;
         li.appendChild(author);
       }
-  
-      li.style.paddingTop = '1rem'
-      reviewsList.appendChild(li)
-    })
-  }
 
-  renderReviews('forward')
+      li.style.paddingTop = '1rem';
+      reviewsList.appendChild(li);
+    });
+  };
+
+  renderReviews('forward');
 
   const paginationButtonBack = document.querySelector('.paginationButtonBack');
   paginationButtonBack.addEventListener('click', async () => {
     renderReviews('back');
-    // if(reviewsData.length < (page_number * page_size)) {
-    //   paginationButton.style.display = 'none';
-    // }
-    if(page_number < 3) {
+    if (page_number < 2) {
       paginationButtonBack.disabled = true;
     }
     paginationButtonForward.disabled = false;
-  });  
+  });
+  paginationButtonBack.disabled = true;
 
-  const paginationButtonForward = document.querySelector('.paginationButtonForward');
+  const paginationButtonForward = document.querySelector(
+    '.paginationButtonForward'
+  );
   paginationButtonForward.addEventListener('click', async () => {
     renderReviews('forward');
     paginationButtonBack.disabled = false;
 
-    if(reviewsData.length < (page_number * page_size)) {
+    if (reviewsData.length <= page_number * page_size) {
       paginationButtonForward.disabled = true;
     }
-  
-  });  
-
-  
-  
-
-
-
-}
+  });
+};
 
 renderReviews();
